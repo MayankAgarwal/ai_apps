85d0fb99f59b40e749faee1b5b1a3da9bf8f30c2
Mayank Agarwal <mayank.agarwal0312@gmail.com>
2017-05-24 03:09:25 -0400

    Compute G and H values for each state

--- assets/js/a_star_sliding_puzzle.js
+++ assets/js/a_star_sliding_puzzle.js
@@ -1,8 +1,9 @@
-function State(goal_state, initial_state, parent)
+function State(goal_state, initial_state, parent_state)
 {
 	this.GOAL_STATE = goal_state;
 	this.current_state = initial_state;
-	this.parent = parent;
+	this.parent_state = parent_state;
+	this.dim = parseInt(Math.sqrt(this.current_state.length + 1));
 
 	// Check if the current state is the goal state
 	this.isGoal = function(){
@@ -17,22 +18,21 @@ function State(goal_state, initial_state, parent)
 				return false;
 
 		return true;
-	}
+	};
 
 	this.getNextStates = function() {
 
 		next_states = [];
 
-		var dim = parseInt(Math.sqrt(this.current_state.length + 1));
 		var empty_block_idx = this.current_state.indexOf(0);
 
 		if (empty_block_idx < 0)
 			throw "Empty piece not found in puzzle";
 
-		var row = parseInt(empty_block_idx/dim)
-		var col = empty_block_idx % dim;
+		var row = parseInt(empty_block_idx/this.dim)
+		var col = empty_block_idx % this.dim;
 
-		var adj_idxs = getAdjacentIdxs(row, col, dim);
+		var adj_idxs = getAdjacentIdxs(row, col, this.dim);
 
 		for (var i in adj_idxs)
 		{
@@ -47,7 +47,7 @@ function State(goal_state, initial_state, parent)
 		}
 
 		return next_states;
-	}
+	};
 
 	var getAdjacentIdxs = function(row, col, dim) {
 		res = []
@@ -67,7 +67,44 @@ function State(goal_state, initial_state, parent)
 		}
 		
 		return res;
-	}
+	};
+
+	this.get_G_value = function() {
+		
+		if (typeof this.parent_state !== 'undefined')
+			return this.parent_state.g_val + 1;
+
+		return 0;
+	};
+
+	this.get_H_value = function() {
+
+		var h_val = 0;
+
+		for(var i=0; i < this.current_state.length; i++)
+		{
+			var curr_val = this.current_state[i]
+
+			if (curr_val === 0)
+				continue;
+
+			var goal_idx = this.GOAL_STATE.indexOf(curr_val);
+
+			var row1 = parseInt(i/this.dim);
+			var col1 = i % this.dim;
+
+			var row2 = parseInt(goal_idx/this.dim);
+			var col2 = goal_idx % this.dim;
+
+			h_val += Math.abs(row1 - row2) + Math.abs(col1 - col2)
+		}
+
+		return h_val;
+	};
+
+	this.g_val = this.get_G_value();
+	this.h_val = this.get_H_value();
+	this.f_val = this.g_val + this.h_val;
 }
 
 function Puzzle(count)
